f8f452ae0aa02b8ccb5d7aa52d541d3847bc412c
karolsteve-acn <karol.s.tchatchouang@accenture.com>
2026-06-30 17:33:16 +0200

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int l = 0;
+        int r = s.length() - 1;
+        while(l < r) {
+            while(!Character.isLetterOrDigit(s.charAt(l)) && l < r) l++;
+            while(!Character.isLetterOrDigit(s.charAt(r)) && r > l) r--;
+            if(Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
