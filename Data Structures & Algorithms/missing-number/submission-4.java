850b087b7067623affb925756b559d03b0737326
karolsteve-acn <karol.s.tchatchouang@accenture.com>
2026-07-06 10:25:52 +0200

    Add: missing-number - submission-4

---
+++ Data Structures & Algorithms/missing-number/submission-4.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int m = nums.length;
+        for(int i = 0; i < nums.length; i++) {
+            m ^= nums[i] ^ i;
+        }
+        return m;
+    }
+}
